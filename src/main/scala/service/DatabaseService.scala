fecbabd1c133e23a5ac3b4d8a69a4ef183d21391
Stefan Aleksic <stefan@stefanaleksic.com>
2016-08-04 06:01:13 -0400

    Create database service class to read/write from the database.

--- src/main/scala/service/DatabaseService.scala
+++ src/main/scala/service/DatabaseService.scala
@@ -1,8 +1,29 @@
 package service
 
+import config.CookieJarConfig
+import models.Cookie.CookieTable
+import models.{Cookie, User}
+import models.User.UserTable
+import slick.driver.JdbcDriver
+
+import slick.driver.MySQLDriver.api._
 /**
   * Created by coldsauce on 8/4/16.
   */
-class DatabaseService {
+class DatabaseService(database: Database) {
+  def createSchema()= {
+    database.run((UserTable.users.schema ++ CookieTable.cookies.schema).create)
+  }
+  def insertUser(user: User) = {
+    database.run(UserTable.users.insertOrUpdate(user))
+  }
+  def insertCookie(cookie: Cookie) = {
+    database.run(CookieTable.cookies.insertOrUpdate((cookie)))
+  }
+}
 
+object DatabaseService {
+  def createFromConfig(config: CookieJarConfig): DatabaseService = {
+    new DatabaseService(config.database)
+  }
 }
